cb02a44c44821a5e19b770f482f74017917b18f5
CollinLapointe <collinlar11@gmail.com>
2026-03-31 09:37:42 -0400

    Initial project setup with classes

---
+++ Gunpla_Checklist/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Gunpla_Checklist
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
